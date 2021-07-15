814da4d97c3fa196788b515de70fa653002f690c
github username <sharnellgrant@gmail.com>
2021-07-14 19:42:05 -0700

    commit tree be2cded6b6ff11141fc943ea7e8d25dadbb11669

--- index.js
+++ index.js
@@ -13,5 +13,14 @@ const tutorials = [
 
 const titleCased = () => {
   // use the .map method on the tutorials to return a new array
-  return tutorials
+  return tutorials.map(title => {
+    const words = title.split(' ');
+    const capitalizedWords =
+      words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+    const newTitle = capitalizedWords.join(' ');
+    return newTitle;
+  });
+  
+
 }
+
